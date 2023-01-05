ec1009566652aecbad12eda9528fa598d3ffd547
kflores <kevin.flores@alignet.com>
2023-01-05 18:36:04 -0500

    Se agrega comentario.

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        "Editado por Kevin Flores"
+
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
